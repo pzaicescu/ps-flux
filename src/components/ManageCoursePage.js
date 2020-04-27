f402f4d783c67c845db199f00229f85c59f9ea1a
unknown <p.zaicescu@gmail.com>
2020-04-27 14:55:04 +0300

    adding forms

---
+++ src/components/ManageCoursePage.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
+
+const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  function handleTitleChange(event) {
+    const updatedCourse = { ...course, title: event.target.value };
+    setCourse(updatedCourse);
+  }
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+      <CourseForm course={course} onTitleChange={handleTitleChange} />
+    </>
+  );
+};
+
+export default ManageCoursePage;
